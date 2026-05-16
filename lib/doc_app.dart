27e15a79e627b26d7e8170ac4b373c62a0a0f878
Amr Reda <amr.reda2014@yahoo.com>
2026-05-16 19:50:56 +0300

    add colors and theme

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -1,4 +1,6 @@
 import 'package:doctor_app_advanced/core/routing/app_routes.dart';
+import 'package:doctor_app_advanced/core/routing/routes.dart';
+import 'package:doctor_app_advanced/core/theming/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -13,6 +15,10 @@ class DocApp extends StatelessWidget {
       splitScreenMode: true,
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: AppColors.primaryColor
+        ),
+        initialRoute: Routes.onboardingScreen,
         onGenerateRoute: AppRoutes.generateRoute,
       
       ),
