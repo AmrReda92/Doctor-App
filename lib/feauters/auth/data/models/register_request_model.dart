774d3812213f17236ffee1ad69e4ff3a4f7dcca0
Amr Reda <amr.reda2014@yahoo.com>
2026-05-22 22:05:29 +0300

    added RegisterRequestModel

---
+++ lib/feauters/auth/data/models/register_request_model.dart
@@ -0,0 +1,41 @@
+class RegisterRequestModel {
+  final String name;
+  final String email;
+  final String phone;
+  final String gender;
+  final String password;
+  final String confirmPassword;
+
+  RegisterRequestModel({
+    required this.phone,
+    required this.gender,
+    required this.confirmPassword,
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+
+ factory RegisterRequestModel.fromJson(Map<String,dynamic> json) {
+   return RegisterRequestModel(
+       phone: json["phone"],
+       gender: json["gender"],
+       confirmPassword: json["password_confirmation"],
+       name: json["name"],
+       email: json["email"],
+       password: json["password"],
+   );
+ }
+
+ Map<String,dynamic> toJson(){
+   return {
+     "name":name,
+      "email":email,
+      "phone":phone,
+      "gender":gender,
+     "password": password,
+     "password_confirmation":  confirmPassword,
+
+   };
+ }
+
+}
