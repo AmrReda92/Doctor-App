7c218a45dc4424f0cc1b4b7cf914f805eb243913
Amr Reda <amr.reda2014@yahoo.com>
2026-05-24 02:05:10 +0300

    add await ScreenUtil.ensureScreenSize

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:doctor_app_advanced/core/networking/dio_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'doc_app.dart';
 
 void main() async{
+  await ScreenUtil.ensureScreenSize();
    WidgetsFlutterBinding.ensureInitialized();
   await DioHelper.init();
   runApp(const DocApp());
