b3591cde338715663d860e6dda95a5592cb766e3
Amr Reda <amr.reda2014@yahoo.com>
2026-05-23 03:50:54 +0300

    add login request model

--- lib/core/networking/api_constants.dart
+++ lib/core/networking/api_constants.dart
@@ -1,4 +1,5 @@
 class ApiConstants{
   static const String baseUrl = "https://vcare.integration25.com/api/";
   static const String registerEndPoint = "auth/register";
+  static const String loginEndPoint = "auth/login";
 }
